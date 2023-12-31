a2093d54600d8668e77161bce6daa6dd9ed5fab5
William Issac Whitaker <70265801+WillBytes@users.noreply.github.com>
2023-12-31 01:34:40 +0000

    First Version

---
+++ src/main/java/org/nuevaterra/time/Retrieval.java
@@ -0,0 +1,81 @@
+package org.nuevaterra.time;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.server.ServerLifecycleHooks;
+import org.slf4j.Logger;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+import static net.minecraft.core.RegistryAccess.LOGGER;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Retrieval.MODID)
+public class Retrieval {
+
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "nttimeretrieval";
+    // Directly reference a slf4j logger
+
+    public Retrieval() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+    @SubscribeEvent
+    public void onServerTick(TickEvent.ServerTickEvent event) {
+        if (event.phase == TickEvent.Phase.START) {
+            long time = ServerLifecycleHooks.getCurrentServer().overworld().getDayTime();
+            writeTimeToFile(time);
+        }
+    }
+
+    private void writeTimeToFile(long time) {
+        try (FileWriter writer = new FileWriter("time.txt")) {
+            writer.write(String.valueOf(time));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        // Some common setup code
+        LOGGER.info("HELLO FROM COMMON SETUP");
+        LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        // Do something when the server starts
+        LOGGER.info("HELLO from server starting");
+    }
+}
